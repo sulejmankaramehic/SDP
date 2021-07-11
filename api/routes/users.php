02c1d9e05193d1a7a457b24ca127e0cb021848ab
sulejmankaramehic <sulejman.karamehic@stu.ibu.edu.ba>
2021-07-11 17:34:17 +0200

    fixed register route documentation

--- api/routes/users.php
+++ api/routes/users.php
@@ -1,20 +1,20 @@
 <?php
 
 /**
- * @OA\Post(path="/users/register", tags={"Register"},
- * @OA\RequestBody(
-    * description="Main user info",
-    * required=true,
-        * @OA\MediaType(mediaType="application/json",
-            * @OA\Schema(
-                * @OA\Property(property="email", type="string", example="", description="Type in email"),
-                * @OA\Property(property="password", type="string", example="", description="Type in password"),
-                * @OA\Property(property="first_name", type="string", example="", description="Type in name"),
-                * @OA\Property(property="username", type="string", example="", description="Type in username"),
-                * @OA\Property(property="last_name", type="string", example="", description="Type in surname") ) ) ),
- * @OA\Response(response="200", description="Register")
+ * @OA\Post(path="/users/register", tags={"users"},
+ *   @OA\RequestBody(description="Basic user info", required=true,
+ *       @OA\MediaType(mediaType="application/json",
+ *    			@OA\Schema(
+ *     				 @OA\Property(property="name", required="true", type="string", example="First Name",	description="Name of the user`" ),
+ *     				 @OA\Property(property="last_name", required="true", type="string", example="Last Name",	description="Surname of the user`" ),
+ *     				 @OA\Property(property="username", required="true", type="string", example="Username",	description="Username`" ),
+ *    				 @OA\Property(property="email", required="true", type="string", example="myemail@gmail.com",	description="User's email address" ),
+ *             @OA\Property(property="password", required="true", type="string", example="12345",	description="Password" )
+ *          )
+ *       )
+ *     ),
+ *  @OA\Response(response="200", description="Message that user has been created.")
  * )
- *
  */
 Flight::route('POST /users/register', function(){
   $data = Flight::request()->data->getData();
