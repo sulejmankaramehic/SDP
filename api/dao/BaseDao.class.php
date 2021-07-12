c9688900f16ad18c25fb7bd5c69230cc41bf2acd
sulejmankaramehic <sulejman.karamehic@stu.ibu.edu.ba>
2021-07-12 20:20:51 +0200

    added db port

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -41,7 +41,7 @@ class BaseDao {
     $this->table = $table;
 
     try {
-      $this->connection = new PDO("mysql:host=".Config::DB_HOST().";dbname=".Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
+      $this->connection = new PDO("mysql:host=".Config::DB_HOST().";port=".Config::DB_PORT().";dbname=".Config::DB_SCHEME(), Config::DB_USERNAME(), Config::DB_PASSWORD());
       $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       $this->connection->setAttribute(PDO::ATTR_AUTOCOMMIT, 0);
       } catch(PDOException $e) {
